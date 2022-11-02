f5447f195fdbff71ce58c35bc0409fe32af25fcc
ajayshik <ajayshik@DVCINH6106728.corp.amdocs.com>
2022-11-02 16:40:49 +0530

    practical1

---
+++ src/practical1/AreaOfCircle.java
@@ -0,0 +1,15 @@
+package practical1;
+import java.util.Scanner;
+public class AreaOfCircle {
+	public static void main(String args[]){
+	      int radius;
+	      double area;
+	      @SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+	      System.out.println("Enter the radius of the circle ::");
+	      radius = sc.nextInt();
+	      area = (radius*radius)*Math.PI;
+	      System.out.println("Area of the circle is ::"+area);
+	   }
+
+}
---
+++ src/practical1/Main.java
@@ -0,0 +1,20 @@
+package practical1;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner myObj=new Scanner(System.in);
+System.out.println("Please Enter name ,age and salary");
+String name=myObj.nextLine();
+int age=myObj.nextInt();
+double salary=myObj.nextDouble();
+System.out.println("Name:"+ name);
+		System.out.println("Age:"+ age);
+				System.out.println("Salary"+ salary);	
+				
+				
+				
+				
+	}
+}
